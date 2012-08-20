18ba887787642dbdad91081b1129f94eff30264b
Brian Michel <brian.michel@gmail.com>
2012-08-19 20:54:52 -0400

    Initial Commit

---
+++ Imgur Roulette/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  Imgur Roulette
+//
+//  Created by Brian Michel on 8/19/12.
+//  Copyright (c) 2012 Foureyes. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
